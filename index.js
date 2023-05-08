092faa4d9acc6dc75f4182ac9c145c6b33421834
Drew Daniels <daniels_drew@protonmail.com>
2023-05-07 19:47:07 -0500

    add styling, functionality to send messages to iframe asynchronously

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { Parent } from 'https://unpkg.com/liaison-core@latest/lib/index.js'
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const parent = Parent({
     iframeOpts: {
         id: 'iframe',
@@ -9,31 +13,31 @@ const parent = Parent({
         classes: ['iframe'],
     },
     effects: {
-        sayHiFromParent: () => console.log('Hello from Parent!')
+        sayHiFromParentSync: () => {
+            const messagesList = document.getElementById('parent-messages-sync');
+            const message = document.createElement('li');
+            message.innerText = 'Synchronous message received from iframe';
+            message.classList.add('message');
+            messagesList.appendChild(message);
+        },
+        sayHiFromParentAsync: async () => {
+            await timeout(3000);
+            const messagesList = document.getElementById('parent-messages-async');
+            const message = document.createElement('li');
+            message.innerText = 'Asynchronous message received from iframe';
+            message.classList.add('message');
+            messagesList.appendChild(message);
+        }
     }
 });
 parent.init();
 
-const button = document.getElementById('parent-btn');
+const button = document.getElementById('parent-btn-sync');
 button.onclick = () => {
-    parent.callIFrameEffect({ name: 'sayHiFromIFrame', args: {} })
+    parent.callIFrameEffect({ name: 'sayHiFromIFrame', args: {} });
 }
 
-const p2 = Parent({
-    iframeOpts: {
-        id: 'iframe2',
-        containerId: 'iframe-container-2',
-        src: 'http://localhost:5502',
-        classes: ['iframe'],
-    },
-    effects: {
-        sayHiFromParent: () => console.log('Hello from parent?')
-    }
-})
-
-p2.init();
-
-const button2 = document.getElementById('parent-btn-2');
-button2.onclick = () => {
-    p2.callIFrameEffect({ name: 'sayHiFromIFrame', args: {} })
+const buttonAsync = document.getElementById('parent-btn-async');
+buttonAsync.onclick = () => {
+    parent.callIFrameEffect({ name: 'sayHiFromIFrameAsync', args: {} });
 }
